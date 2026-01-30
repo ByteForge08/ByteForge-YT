76c1db85e2285d1ddfa033c42172e0942c827e5a
Mrboombastic <pedromiguelgomesdias2109@gmail.com>
2026-01-29 23:07:21 -0300

    app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_info():
     'headers': {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     }
-}
+    }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -70,3 +70,4 @@ def get_info():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
